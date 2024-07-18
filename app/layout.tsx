419f0cf4dd10d0627ae3d27d3f702b1736653eb2
Sarino MALALANIRINA <sarino.malalanirina@esti.mg>
2024-07-18 10:31:22 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Anek_Telugu } from "next/font/google";
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { cn } from "@/lib/utils";
+
+const AnekTelugu = Anek_Telugu({ 
+  subsets: ["latin"],
+  variable: "--font-caption",
+ });
+
+export const metadata: Metadata = {
+  title: "Sarino Malalanirina . Software Engineer",
+  description: "Developer Java, React, TailwindCSS, Next.js, and TypeScript",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-foreground")}>{children}</body>
+    </html>
+  );
+}
